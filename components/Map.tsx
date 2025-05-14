05cb216094fd835d3702c57eca58bf59e0c6f91d
root <nathan3108chan@gmail.com>
2025-05-14 11:36:49 +1000

    Swapped to animated custom markers

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import {
+  Alert,
+  Animated,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { Colors } from "../constants/Colors";
 import useLocation from "../hooks/useLocation";
@@ -24,10 +31,23 @@ interface Place {
   };
 }
 
+interface MarkerPosition {
+  x: number;
+  y: number;
+}
+
 const Map: React.FC<MapProps> = ({ searchQuery, coordinates }) => {
   const [nearbyPlaces, setNearbyPlaces] = useState<Place[]>([]);
   const [isLoadingPlaces, setIsLoadingPlaces] = useState(false);
+  const [markerPositions, setMarkerPositions] = useState<{
+    [key: number]: MarkerPosition;
+  }>({});
+  const [overlayWidths, setOverlayWidths] = useState<{ [key: number]: number }>(
+    {}
+  );
   const { latitude, longitude, error, loading } = useLocation();
+  const mapRef = useRef<MapView>(null);
+  const updateTimeout = useRef<ReturnType<typeof setTimeout>>(null);
 
   useEffect(() => {
     const fetchNearbyPlaces = async () => {
@@ -107,59 +127,87 @@ const Map: React.FC<MapProps> = ({ searchQuery, coordinates }) => {
 
     fetchNearbyPlaces();
   }, [coordinates, latitude, longitude]);
+
+  const debouncedUpdateMarkerPositions = useCallback(() => {
+    if (updateTimeout.current) {
+      clearTimeout(updateTimeout.current);
+    }
+
+    updateTimeout.current = setTimeout(() => {
+      if (!mapRef.current) return;
+      nearbyPlaces.forEach((place, index) => {
+        mapRef.current?.pointForCoordinate(place.location).then((point) => {
+          setMarkerPositions((prev) => ({
+            ...prev,
+            [index]: point,
+          }));
+        });
+      });
+    }, 16);
+  }, [nearbyPlaces, markerPositions]);
+
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
         {!loading && !isLoadingPlaces && latitude && longitude ? (
-          <MapView
-            provider={PROVIDER_GOOGLE}
-            style={StyleSheet.absoluteFillObject}
-            region={{
-              latitude: coordinates?.latitude || latitude,
-              longitude: coordinates?.longitude || longitude,
-              latitudeDelta: 0.005,
-              longitudeDelta: 0.005,
-            }}
-            showsUserLocation={true}
-          >
-            {/* Center Pin */}
-            {/* <Marker
-              coordinate={{
+          <View style={{ flex: 1 }}>
+            <MapView
+              ref={mapRef}
+              provider={PROVIDER_GOOGLE}
+              style={StyleSheet.absoluteFillObject}
+              region={{
                 latitude: coordinates?.latitude || latitude,
                 longitude: coordinates?.longitude || longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
               }}
-              anchor={{ x: 0.5, y: 1 }}
+              showsUserLocation={true}
+              onLayout={debouncedUpdateMarkerPositions}
+              onRegionChange={debouncedUpdateMarkerPositions}
             >
-              <View style={styles.centerPinContainer}>
-                <View style={styles.centerPin} />
-                <View style={styles.centerPinShadow} />
-              </View>
-            </Marker> */}
-
-            {nearbyPlaces.map((place, index) => (
-              <Marker
-                key={index}
-                coordinate={{
-                  latitude: place.location.latitude,
-                  longitude: place.location.longitude,
-                }}
-                anchor={{ x: 0.5, y: 1 }}
-                flat={true}
-              >
-                <View>
-                  <View style={styles.markerContainer}>
-                    <Text style={styles.markerText}>
-                      {place.displayName.text}
-                    </Text>
-                  </View>
-                  <View style={styles.pinContainer}>
-                    <View style={styles.pin} />
-                    <View style={styles.pinShadow} />
-                  </View>
-                </View>
-              </Marker>
-            ))}
-          </MapView>
+              {nearbyPlaces.map((place, index) => (
+                <Marker
+                  key={index}
+                  coordinate={{
+                    latitude: place.location.latitude,
+                    longitude: place.location.longitude,
+                  }}
+                >
+                  <View style={styles.pin} />
+                </Marker>
+              ))}
+            </MapView>
+            {nearbyPlaces.map(
+              (place, index) =>
+                markerPositions[index] && (
+                  <Animated.View
+                    key={`overlay-${index}`}
+                    style={[
+                      styles.testBox,
+                      {
+                        position: "absolute",
+                        left:
+                          markerPositions[index].x -
+                          (overlayWidths[index]
+                            ? overlayWidths[index] / 2
+                            : 60),
+                        top: markerPositions[index].y - 60,
+                      },
+                    ]}
+                    onLayout={(event) => {
+                      const { width } = event.nativeEvent.layout;
+                      setOverlayWidths((prev) => ({ ...prev, [index]: width }));
+                    }}
+                  >
+                    <TouchableOpacity
+                      onPress={() => Alert.alert(place.displayName.text)}
+                    >
+                      <Text>{place.displayName.text}</Text>
+                    </TouchableOpacity>
+                  </Animated.View>
+                )
+            )}
+          </View>
         ) : (
           <View
             style={{
@@ -192,28 +240,16 @@ const styles = StyleSheet.create({
     fontSize: 12,
     textAlign: "center",
   },
-  markerContainer: {
+  testBox: {
+    position: "absolute",
+    top: 100,
+    left: 100,
     backgroundColor: "white",
-    padding: 8,
-    borderRadius: 6,
+    padding: 10,
+    borderRadius: 5,
     borderWidth: 1,
-    borderColor: "#ccc",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  markerText: {
-    fontSize: 12,
-    textAlign: "center",
-    minWidth: 80,
-  },
-  pinContainer: {
-    width: 30,
-    height: 30,
-    alignItems: "center",
-    justifyContent: "center",
+    borderColor: "black",
+    zIndex: 1000,
   },
   pin: {
     width: 20,
@@ -227,15 +263,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-  },
-  pinShadow: {
-    position: "absolute",
-    bottom: 0,
-    width: 10,
-    height: 10,
-    backgroundColor: "rgba(0, 0, 0, 0.1)",
-    borderRadius: 5,
-    transform: [{ scaleX: 2 }],
   },
 });
 
